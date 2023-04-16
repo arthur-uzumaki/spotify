de64fbd681aa2d4283782c633cbbee39df7e1aba
Arthur Sousa <102182959+Arttanjeiro@users.noreply.github.com>
2023-04-15 23:01:57 -0300

    Acabando o menu do spotify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
-
+import { Home as HomeIcon, Library, Search } from 'lucide-react'
 export default function Home() {
   return (
     <div className="h-screen flex flex-col  ">
@@ -7,14 +7,37 @@ export default function Home() {
       <div className="flex flex-1 ">
 
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav>
-            <a href="">Home</a>
+          <div className='flex items-center gap-2'>
+              <div className='w-3 h-3 bg-red-500 rounded-full'/>
+              <div className='w-3 h-3 bg-yellow-500 rounded-full'/>
+              <div className='w-3 h-3 bg-green-500 rounded-full'/>
+          </div>
 
-            <a href="">Search</a>
+          <nav className='space-y-5 mt-10'>
+            <a href="" className='flex items-center gap-3 text-sm  font-semibold text-zinc-00'>
+              <HomeIcon/>
+              Home
+              </a>
 
-            <a href="">Your Library</a>
+            <a href="" className='flex items-center gap-3 text-sm  font-semibold text-zinc-100'>
+              <Search/>
+              Search
+              </a>
+
+            <a href="" className='flex items-center gap-3 text-sm  font-semibold text-zinc-100'>
+              <Library/>
+              Your Library
+              </a>
             
           </nav>
+
+            <nav className='mt-6 pt-10 border-t border-zinc-800 p-6 flex flex-col gap-3'>
+              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Hot Hits Brasil</a>
+              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Aniver Funk</a>
+              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>My Playlist #1</a>
+              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Top Brasil</a>
+            </nav>
+
         </aside>
 
         <main className="flex-1 p-6">
