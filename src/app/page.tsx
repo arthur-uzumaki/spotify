f5f8704a1f2fd33d817060fa1e86ac9295e381ed
Arthur Sousa <102182959+Arttanjeiro@users.noreply.github.com>
2023-04-16 22:33:14 -0300

    Criando album na main com grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 
-import { Home as HomeIcon, Library, Search } from 'lucide-react'
+import { ChevronLeft, ChevronRight, Home as HomeIcon, Library, Play, Search } from 'lucide-react'
+import Image from 'next/image'
+
+import album from '../assets/snop dog.jpg'
+
 export default function Home() {
   return (
     <div className="h-screen flex flex-col  ">
@@ -8,44 +12,106 @@ export default function Home() {
 
         <aside className="w-72 bg-zinc-950 p-6">
           <div className='flex items-center gap-2'>
-              <div className='w-3 h-3 bg-red-500 rounded-full'/>
-              <div className='w-3 h-3 bg-yellow-500 rounded-full'/>
-              <div className='w-3 h-3 bg-green-500 rounded-full'/>
+            <div className='w-3 h-3 bg-red-500 rounded-full' />
+            <div className='w-3 h-3 bg-yellow-500 rounded-full' />
+            <div className='w-3 h-3 bg-green-500 rounded-full' />
           </div>
 
           <nav className='space-y-5 mt-10'>
             <a href="" className='flex items-center gap-3 text-sm  font-semibold text-zinc-00'>
-              <HomeIcon/>
+              <HomeIcon />
               Home
-              </a>
+            </a>
 
             <a href="" className='flex items-center gap-3 text-sm  font-semibold text-zinc-100'>
-              <Search/>
+              <Search />
               Search
-              </a>
+            </a>
 
             <a href="" className='flex items-center gap-3 text-sm  font-semibold text-zinc-100'>
-              <Library/>
+              <Library />
               Your Library
-              </a>
-            
+            </a>
+
           </nav>
 
-            <nav className='mt-6 pt-10 border-t border-zinc-800 p-6 flex flex-col gap-3'>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Hot Hits Brasil</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Aniver Funk</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>My Playlist #1</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Top Brasil</a>
-            </nav>
+          <nav className='mt-6 pt border-t border-zinc-800 p-6 flex flex-col gap-3'>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Hot Hits Brasil</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Aniver Funk</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>M roundedy #1</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Top Brasil</a>
+          </nav>
 
         </aside>
 
         <main className="flex-1 p-6">
-          main
+
+          <div className='flex items-center gap-3'>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronLeft />
+            </button>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className='font-semibold text-3xl mt-4'>Good Afternoon</h1>
+
+          <div className='grid grid-cols-3 gap-4 mt-5'>
+              <a className='bg-white/5 rounded group  flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+                <Image src={album} alt='Capa da musica' width={100} height={100}/>
+                  <strong>I Wanna Thank Me</strong>
+                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                    <Play/>
+                  </button>
+                </a>
+
+              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+                <Image src={album} alt='Capa da musica' width={100} height={100}/>
+                  <strong>I Wanna Thank Me</strong>
+                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                    <Play/>
+                  </button>
+                </a>
+
+              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+                <Image src={album} alt='Capa da musica' width={100} height={100}/>
+                  <strong>I Wanna Thank Me</strong>
+                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                    <Play/>
+                  </button>
+                </a>
+
+              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+                <Image src={album} alt='Capa da musica' width={100} height={100}/>
+                  <strong>I Wanna Thank Me</strong>
+                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                    <Play/>
+                  </button>
+                </a>
+
+              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+                <Image src={album} alt='Capa da musica' width={100} height={100}/>
+                  <strong>I Wanna Thank Me</strong>
+                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                    <Play/>
+                  </button>
+                </a>
+
+              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+                <Image src={album} alt='Capa da musica' width={100} height={100}/>
+                  <strong>I Wanna Thank Me</strong>
+                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                    <Play/>
+                  </button>
+                </a>
+
+          </div>
+
         </main>
 
       </div>
-      
+
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6 ">
         footer
       </footer>
