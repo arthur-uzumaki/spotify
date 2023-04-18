4bfe650dbfc93a8889f0292c65fffc1ed3f04e34
Arthur Sousa <102182959+Arttanjeiro@users.noreply.github.com>
2023-04-17 21:50:48 -0300

    Terminaando o conteúdo do spotify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,53 +58,94 @@ export default function Home() {
           <h1 className='font-semibold text-3xl mt-4'>Good Afternoon</h1>
 
           <div className='grid grid-cols-3 gap-4 mt-5'>
-              <a className='bg-white/5 rounded group  flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-                <Image src={album} alt='Capa da musica' width={100} height={100}/>
-                  <strong>I Wanna Thank Me</strong>
-                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                    <Play/>
-                  </button>
-                </a>
+            <a className='bg-white/5 rounded group  flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src={album} alt='Capa da musica' width={100} height={100} />
+              <strong>I Wanna Thank Me</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
 
-              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-                <Image src={album} alt='Capa da musica' width={100} height={100}/>
-                  <strong>I Wanna Thank Me</strong>
-                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                    <Play/>
-                  </button>
-                </a>
+            <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src={album} alt='Capa da musica' width={100} height={100} />
+              <strong>I Wanna Thank Me</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
 
-              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-                <Image src={album} alt='Capa da musica' width={100} height={100}/>
-                  <strong>I Wanna Thank Me</strong>
-                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                    <Play/>
-                  </button>
-                </a>
+            <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src={album} alt='Capa da musica' width={100} height={100} />
+              <strong>I Wanna Thank Me</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
 
-              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-                <Image src={album} alt='Capa da musica' width={100} height={100}/>
-                  <strong>I Wanna Thank Me</strong>
-                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                    <Play/>
-                  </button>
-                </a>
+            <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src={album} alt='Capa da musica' width={100} height={100} />
+              <strong>I Wanna Thank Me</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
 
-              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-                <Image src={album} alt='Capa da musica' width={100} height={100}/>
-                  <strong>I Wanna Thank Me</strong>
-                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                    <Play/>
-                  </button>
-                </a>
+            <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src={album} alt='Capa da musica' width={100} height={100} />
+              <strong>I Wanna Thank Me</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
 
-              <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-                <Image src={album} alt='Capa da musica' width={100} height={100}/>
-                  <strong>I Wanna Thank Me</strong>
-                  <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                    <Play/>
-                  </button>
-                </a>
+            <a className='bg-white/5 rounded flex group items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src={album} alt='Capa da musica' width={100} height={100} />
+              <strong>I Wanna Thank Me</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
+
+          </div>
+
+          <h2 className='font-semibold text-2xl mt-10'> Mode for Arthur </h2>
+
+          <div className='grid grid-cols-8 gap-4 mt-4'>
+
+            <a href='' className='bg-white/5 p-2 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+              
+              <Image src={album} alt='Capa da musica' width={120} height={120} quality={100} className='w-full' />
+                <strong className='font-semibold '>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-400 '>Snoop Dogs in red and more</span>
+            </a>
+
+            <a href='' className='bg-white/5 p-2 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+              
+              <Image src={album} alt='Capa da musica' width={120} height={120} quality={100} className='w-full' />
+                <strong className='font-semibold '>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-400 '>Snoop Dogs in red and more</span>
+            </a>
+
+            <a href='' className='bg-white/5 p-2 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+              
+              <Image src={album} alt='Capa da musica' width={120} height={120} quality={100} className='w-full' />
+                <strong className='font-semibold '>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-400 '>Snoop Dogs in red and more</span>
+            </a>
+
+            <a href='' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+              
+              <Image src={album} alt='Capa da musica' width={120} height={120} quality={100} className='w-full' />
+                <strong className='font-semibold '>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-400 '>Snoop Dogs in red and more</span>
+            </a>
+
+            <a href='' className='bg-white/5 p-2 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+              
+              <Image src={album} alt='Capa da musica' width={120} height={120} quality={100} className='w-full' />
+                <strong className='font-semibold '>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-400 '>Snoop Dogs in red and more</span>
+            </a>
 
           </div>
 
